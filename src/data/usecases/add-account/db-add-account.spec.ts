a70892a1b05d8a306546db89d3235e307a0a87fb
EduYoso <eduardocs2000@hotmail.com>
2024-07-10 21:33:13 -0300

    refactor: move encrypter creation to a factory helper method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -6,13 +6,18 @@ interface SutTypes {
   encrypterStub: Encrypter;
 }
 
-const makeSut = (): SutTypes => {
+const makeEncrypter = (): Encrypter => {
   class EncrypterStub {
     async encrypt(value: string): Promise<string> {
       return "hashed_password";
     }
   }
   const encrypterStub = new EncrypterStub();
+  return encrypterStub;
+};
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypter();
   const sut = new DbAddAccount(encrypterStub);
   return {
     encrypterStub,
